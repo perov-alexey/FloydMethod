90336bed8f9c5eefbdaafc735ee787e4ff84772f
Alexey_Perov <perov-alexey@yandex.ru>
2016-10-27 18:45:28 +0300

    Add drag and drop, connector, connector weight, styles for elements, but hardcoded yet

--- graphBuilder.js
+++ graphBuilder.js
@@ -1,5 +1,32 @@
 jsPlumb.ready(function() {
     var jsPlumbInstance = jsPlumb.getInstance({
-        Container: ".container"
+        Container: ".container",
+        Connector: "StateMachine",
+        Anchor: ["Perimeter", {shape: "Square"}],
+        Endpoint: ["Dot", {cssClass: "transparentEndpoint"}],
+        ConnectionOverlays: [
+            ["Arrow", {location: 1}]
+        ]
+    });
+
+    jsPlumbInstance.connect({
+        source: "first",
+        target: "second",
+        overlays: [
+            ["Custom", {
+                create: function(component) {
+                    var label = $("<input size='1'>");
+                    label.on("keyup", function() {
+                        this.style.width = ((this.value.length || 1) * 7) + 'px';
+                    });
+                    label.keyup();
+                    return label;
+                }
+            }]
+        ]
+    });
+
+    jsPlumbInstance.draggable($(".jsplumb-connected"), {
+        containment:true
     });
 });
